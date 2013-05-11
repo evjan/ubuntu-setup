88f964f733c33ce14d2dbe75c08455c2055e48b3
Peter Evjan <peter.evjan@gmail.com>
2013-05-11 11:44:56 +0000

    Installing tmux

--- 1-sudo-setup.sh
+++ 1-sudo-setup.sh
@@ -6,6 +6,9 @@ apt-get -y --force-yes upgrade
 apt-get -y --force-yes install build-essential
 
 apt-get -y --force-yes install emacs
+
 apt-get -y --force-yes install git
+
+apt-get -y --force-yes install tmux
 
 echo "Done!"
