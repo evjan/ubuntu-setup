93d92d7cea7023b249a4c80afe9a521a66886d37
Peter Evjan <peter.evjan@gmail.com>
2013-05-11 12:37:32 +0000

    Installing emacs 24 from another PPA

--- setup.sh
+++ setup.sh
@@ -11,6 +11,14 @@ sudo apt-get -y --force-yes install git
 
 sudo apt-get -y --force-yes install tmux
 
+sudo apt-get -y --force-yes install curl
+
+sudo apt-get -y --force-yes install make
+
+sudo apt-get -y --force-yes install python-software-properties
+sudo apt-get update
+sudo apt-get -y --force-yes install emacs24
+
 git config --global user.name "Peter Evjan"
 git config --global user.email peter.evjan@gmail.com
 
