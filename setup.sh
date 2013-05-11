3a1832fd778bd7120193d01705696113a28546a9
Peter Evjan <peter.evjan@gmail.com>
2013-05-11 12:53:43 +0000

    Updating to latest release

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 set -e
 
+sudo apt-get -y --force-yes install emacs
+
+sudo emacs /etc/update-manager/release-upgrades
+sudo do-release-upgrade
+
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 
 sudo apt-get -y --force-yes install build-essential
-
-sudo apt-get -y --force-yes install emacs
 
 sudo apt-get -y --force-yes install git
 
@@ -14,10 +17,6 @@ sudo apt-get -y --force-yes install tmux
 sudo apt-get -y --force-yes install curl
 
 sudo apt-get -y --force-yes install make
-
-sudo apt-get -y --force-yes install python-software-properties
-sudo apt-get update
-sudo apt-get -y --force-yes install emacs24
 
 git config --global user.name "Peter Evjan"
 git config --global user.email peter.evjan@gmail.com
